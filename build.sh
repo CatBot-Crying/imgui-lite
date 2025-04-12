41ec403d4fae0e356900afbd5d2b936d78cd8c39
CatBot-Crying <88854387+CatBot-Crying@users.noreply.github.com>
2025-04-12 10:56:20 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 wget https://dl.google.com/android/repository/android-ndk-r28-linux.zip
 
-unzip android-ndk-r28.zip
+unzip android-ndk-r28-linux.zip
 mv android-ndk-r28/ ndk/
 
 ./ndk/build/ndk-build
